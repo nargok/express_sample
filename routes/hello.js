01a84046e183fea7f1bc7e27eecfdb1161272902
Ryoichi Gokan <nargok@gmail.com>
2018-08-14 17:38:34 +0900

    input text & display it

--- routes/hello.js
+++ routes/hello.js
@@ -6,10 +6,18 @@ router.get('/', (req, res, next) => {
   var mail = req.query.mail;
   var data = {
     title: 'Hello!',
-    content: 'あなたの名前は、' + name + '。<br>' +
-              'メールアドレスは、' + mail + 'です。' 
+    content: '※何か書いて送信してください'
   };
   res.render('hello', data);
 });
+
+router.post('/post', (req, res, next) => {
+  var msg = req.body['message'];
+  var data = {
+    title: 'Hello!',
+    content: 'あなたは、「' + msg + '」と送信しました。'
+  };
+  res.render('hello', data)
+})
 
 module.exports = router;
